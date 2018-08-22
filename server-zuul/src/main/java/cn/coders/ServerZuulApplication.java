996ba79f17499eefbbecf7691054ad7fac9083cd
shaorensheng <329917086@qq.com>
2018-08-22 23:02:39 +0800

    提交config、eureka、zuul和base

---
+++ server-zuul/src/main/java/cn/coders/ServerZuulApplication.java
@@ -0,0 +1,16 @@
+package cn.coders;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@EnableZuulProxy
+@EnableDiscoveryClient
+@SpringBootApplication
+public class ServerZuulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerZuulApplication.class, args);
+	}
+}
---
+++ server-zuul/src/main/java/cn/coders/filter/Myfilter.java
@@ -0,0 +1,58 @@
+package cn.coders.filter;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * zuul过滤器
+ *
+ * @author shaorensheng
+ * @date 2018/8/22 15:20
+ */
+@Component
+public class Myfilter extends ZuulFilter {
+    Logger log = LoggerFactory.getLogger(Myfilter.class);
+    /**
+     *
+     * @param
+     * @return :  pre：路由之前
+     *   routing：路由之时
+     *   post： 路由之后
+     *   error：发送错误调用
+     *   filterOrder：过滤的顺序
+     *   shouldFilter：这里可以写逻辑判断，是否要过滤，本文true,永远过滤。
+     *   run：过滤器的具体逻辑。可用很复杂，包括查sql，nosql去判断该请求到底有没有权限访问。
+     * @author:  shaorensheng
+     * @date:   2018/8/22 15:27
+     */
+    @Override
+    public String filterType() {
+        return "pre";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 0;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+        RequestContext ctx = RequestContext.getCurrentContext();
+        HttpServletRequest request = ctx.getRequest();
+        log.info("{}>>>{}",request.getMethod(),request.getRequestURL().toString());
+        String name = request.getParameter("name");
+        log.info("name={}",name);
+        return null;
+    }
+}
